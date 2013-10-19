029cee2b9b2d3769b320b537393c238dd80a68cc
ccs95508 <ccs95508@gmail.com>
2013-10-19 16:20:10 +0800

    fix #10

--- java.js
+++ java.js
@@ -13,17 +13,41 @@ var game = (function() {
             x = Math.floor(Math.random() *800), 
     	    y = Math.floor(Math.random() *800),
     	    r = Math.floor(Math.random() *100);
+            c = Math.floor(Math.random() *6);
 
-        ctx.fillStyle = "#26e60d";
+        if(c==1){
+            ctx.fillStyle = "#26e60d";
+        }
+        else
+            if(c==2){
+            ctx.fillStyle = "yellow";
+        }
+         else
+            if(c==3){
+            ctx.fillStyle = "blue";
+        }
+         else
+            if(c==4){
+            ctx.fillStyle = "green";
+        }
+         else
+            if(c==0){
+            ctx.fillStyle = "#f20818";
+        }
+        else
+            if(c==5){
+            ctx.fillStyle = "white";
+        }
+
         ctx.beginPath();
         ctx.arc(x, y, r, 0, Math.PI * 2 , true);
         ctx.fill();
 
-        if(count>=25){
+        if(count>=10000){
         
         }
         else{
-            timeoutVar = setTimeout(start, 300);
+            timeoutVar = setTimeout(start, 5);
             count = count+1;
         }
 
