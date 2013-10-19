b28bac7a2302867b6673004c9122f1fcb3bcb505
ccs95508 <ccs95508@gmail.com>
2013-10-19 16:58:02 +0800

    try.  fix #11

--- java.js
+++ java.js
@@ -1,7 +1,9 @@
 var game = (function() {
 
         var canvas = document.getElementById('game'),
-            ctx = canvas.getContext('2d');
+            ctx = canvas.getContext('2d'),
+            colors = ['#26e60d','yellow','blue','green','#f20818','#d908f2','#08eaf2'],
+            lenght = colors.length;
 
             canvas.width = 1000;
             canvas.height = 1000;
@@ -13,36 +15,9 @@ var game = (function() {
             x = Math.floor(Math.random() *800), 
     	    y = Math.floor(Math.random() *800),
     	    r = Math.floor(Math.random() *100);
-            c = Math.floor(Math.random() *7);
-
-        if(c==1){
-            ctx.fillStyle = "#26e60d";
-        }
-        else
-            if(c==2){
-            ctx.fillStyle = "yellow";
-        }
-         else
-            if(c==3){
-            ctx.fillStyle = "blue";
-        }
-         else
-            if(c==4){
-            ctx.fillStyle = "green";
-        }
-         else
-            if(c==0){
-            ctx.fillStyle = "#f20818";
-        }
-        else
-            if(c==5){
-            ctx.fillStyle = "white";
-        }
-        else
-            if(c==5){
-            ctx.fillStyle = "black";
-        }
-
+           
+        
+        ctx.fillStyle = colors[Math.floor(Math.random() *7)];
         ctx.beginPath();
         ctx.arc(x, y, r, 0, Math.PI * 2 , true);
         ctx.fill();
