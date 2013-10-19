0bfb151e5d48ad22e2d4620557e8898f2db05adc
ccs95508 <ccs95508@gmail.com>
2013-10-19 15:45:05 +0800

    fix #7

--- java.js
+++ java.js
@@ -1,5 +1,8 @@
-(function() {
-        var canvas = document.getElementById('game'),
+var game = (function() {
+    function start(){
+        var timeoutVar,
+            count=0,
+            canvas = document.getElementById('game'),
             ctx = canvas.getContext('2d'),
             x = Math.floor(Math.random() *200), 
     	    y = Math.floor(Math.random() *500),
@@ -14,5 +17,10 @@
         ctx.arc(x, y, r, 0, Math.PI * 2 , true);
         ctx.fill();
 
-        
+        }
+return{
+    start: start
+}
 }) ();
+
+game.start();
