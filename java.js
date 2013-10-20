5e4066abe47f80302580bebf4951c58b762c6bfe
ccs95508 <ccs95508@gmail.com>
2013-10-20 12:10:06 +0800

    fix #14

--- java.js
+++ java.js
@@ -1,56 +1,68 @@
-var gameModule = (function() {
+/*jslint browser: true, devel: true, closure: true */
 
-        var timeVar,
-            count=0,
+
+
+var gameModule = (function (document) {
+
+    "use strict";
+
+        var timeoutVar,
+            count = 0,
+            x,
+            y,
+            bx,
+            by,
+            br,
             colors = ['#26e60d','yellow','blue','green','#f20818','#d908f2','#08eaf2'],
-            lenght = colors.length;
-
-
+            lenght = colors.length,
             canvas = document.getElementById('game'),
-        ctx = canvas.getContext('2d');
+            ctx = canvas.getContext('2d');
             canvas.width = 2000;
             canvas.height = 1000;
 
-            
-
             function touchEvent(evt){
-                var x = evt.clientX,
-                    y = evt.clientY;
-                    console.log("Clicked:" + x +"," + y);
+                x = evt.clientX;
+                y = evt.clientY;
+                console.log("Clicked:" + x + "," + y);
+
+                var tmp = (bx - x) * (bx - x) + (by - y) * (by - y);
+
+            if(tmp < br * br){
+                console.log("GREAT!!");
             }
+        }
+
 
            function start() {
                 document.getElementById("main").addEventListener("click", touchEvent, false);
                 startGame();                
             }
 
-
     function startGame(){
-            bx = Math.floor(Math.random() *2000), 
-    	    by = Math.floor(Math.random() *900),
-    	    br = Math.floor(Math.random() *200);
-           
-        
+        bx = Math.floor (Math.random() * 2000); 
+        by = Math.floor (Math.random() * 900);
+        br = Math.floor (Math.random() * 200);
         ctx.fillStyle = colors[Math.floor(Math.random() *7)];
         ctx.beginPath();
         ctx.arc(bx, by, br, 0, Math.PI * 2 , true);
         ctx.fill();
 
-        if(count>=9999999999){
-        
+        if (count >= 50) {
         }
-        else{
-            timeoutVar = setTimeout(start, 500);
-            count = count+1;
+        else {
+            timeoutVar = setTimeout(start, 100);
+            count = count + 1;
         }
         }
 
         function gameOver(){
-            console.log("Counter:" + count);        
+            console.log("Counter:" + count);
         }
-return{
+
+return {
     start: start
 }
-}) ();
+
+}(document));
 
 gameModule.start();
