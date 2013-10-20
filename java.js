d2e02abcda0428174117c1f3741d9b8ed20c9a00
ccs95508 <ccs95508@gmail.com>
2013-10-20 11:26:19 +0800

    fix  #13

--- java.js
+++ java.js
@@ -1,39 +1,56 @@
-var game = (function() {
+var gameModule = (function() {
 
-        var canvas = document.getElementById('game'),
-            ctx = canvas.getContext('2d'),
+        var timeVar,
+            count=0,
             colors = ['#26e60d','yellow','blue','green','#f20818','#d908f2','#08eaf2'],
             lenght = colors.length;
 
-            canvas.width = 1000;
+
+            canvas = document.getElementById('game'),
+        ctx = canvas.getContext('2d');
+            canvas.width = 2000;
             canvas.height = 1000;
 
+            
 
-    function start(){
-        var timeoutVar,
-            count=0,
-            x = Math.floor(Math.random() *800), 
-    	    y = Math.floor(Math.random() *800),
-    	    r = Math.floor(Math.random() *100);
+            function touchEvent(evt){
+                var x = evt.clientX,
+                    y = evt.clientY;
+                    console.log("Clicked:" + x +"," + y);
+            }
+
+           function start() {
+                document.getElementById("main").addEventListener("click", touchEvent, false);
+                startGame();                
+            }
+
+
+    function startGame(){
+            bx = Math.floor(Math.random() *2000), 
+    	    by = Math.floor(Math.random() *900),
+    	    br = Math.floor(Math.random() *200);
            
         
         ctx.fillStyle = colors[Math.floor(Math.random() *7)];
         ctx.beginPath();
-        ctx.arc(x, y, r, 0, Math.PI * 2 , true);
+        ctx.arc(bx, by, br, 0, Math.PI * 2 , true);
         ctx.fill();
 
-        if(count>=10000){
+        if(count>=9999999999){
         
         }
         else{
-            timeoutVar = setTimeout(start, 3);
+            timeoutVar = setTimeout(start, 500);
             count = count+1;
         }
+        }
 
+        function gameOver(){
+            console.log("Counter:" + count);        
         }
 return{
     start: start
 }
 }) ();
 
-game.start();
+gameModule.start();
